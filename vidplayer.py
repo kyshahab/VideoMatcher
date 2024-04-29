fb9ba69211c146a3141cd3c867c10a8fa138f653
Caleb Yun <caleb.yun@gmail.com>
2024-04-28 18:09:31 -0700

    Change media player to vlc

--- vidplayer.py
+++ vidplayer.py
@@ -1,24 +1,34 @@
 from ffpyplayer.player import MediaPlayer
 import cv2
-from PyQt5.QtWidgets import QWidget, QPushButton, QVBoxLayout, QLabel
-from PyQt5.QtCore import QTimer, Qt 
-from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtWidgets import QWidget, QPushButton, QVBoxLayout, QFrame
+import vlc
+import sys
+import time
 
 class VidPlayer(QWidget):
     def __init__(self, vidpath, best_frame = 0):
         super().__init__()
         self.vidpath = vidpath
         self.best_frame = best_frame
+
+        self.instance = vlc.Instance('-q')
+        self.mediaplayer = self.instance.media_player_new()
+        self.isPlaying = False
+
         self.setUI()
+
 
     def setUI(self):
         self.setGeometry(100, 100, 500, 500)
         layout = QVBoxLayout()
         self.setLayout(layout)
 
-        self.label = QLabel(self)
-        self.label.setAlignment(Qt.AlignCenter)
-        layout.addWidget(self.label)
+        if sys.platform == "darwin":  # macOS
+            from PyQt5.QtWidgets import QMacCocoaViewContainer
+            self.videoframe = QMacCocoaViewContainer(0)
+        else:
+            self.videoframe = QFrame()
+        layout.addWidget(self.videoframe)
 
         self.play_button = QPushButton('Pause/Play', self)
         self.play_button.clicked.connect(self.play_vid)
@@ -28,60 +38,41 @@ class VidPlayer(QWidget):
         self.reset_button.clicked.connect(self.reset_vid)
         layout.addWidget(self.reset_button)
 
-        self.timer = QTimer(self)
-        self.timer.timeout.connect(self.next_frame)
+        self.media = self.instance.media_new(self.vidpath)
+        self.mediaplayer.set_media(self.media)
+
+        if sys.platform == "win32":
+            self.mediaplayer.set_hwnd(self.videoframe.winId())
+        elif sys.platform == "darwin":
+            self.mediaplayer.set_nsobject(int(self.videoframe.winId()))
 
         self.vidcap = cv2.VideoCapture(self.vidpath)
         if self.vidcap.isOpened():
-            vid_fps = self.vidcap.get(cv2.CAP_PROP_POS_FRAMES)
-            if vid_fps == 0:
-                print("faulty fps")
-                vid_fps = 30
-            self.vidcap.set(cv2.CAP_PROP_POS_FRAMES, self.best_frame)
-            self.player = MediaPlayer(self.vidpath, ff_opts={'sync': 'audio', 'ss': self.best_frame / self.vidcap.get(cv2.CAP_PROP_FPS)})
-            self.next_frame()
-            self.timer.start(int(1000/vid_fps))
+            frames = self.vidcap.get(cv2.CAP_PROP_FRAME_COUNT)
+            self.vidcap.release()
         else:
             print("can't open video")
             return
-        
+
+        self.mediaplayer.play()
+        self.mediaplayer.set_position(self.best_frame / frames)
+        time.sleep(0.02)  # show first frame
+        self.mediaplayer.set_pause(1)
+
 
     def play_vid(self):
-        if self.timer.isActive():
-            self.timer.stop()
-            self.player.set_pause(True)
+        if self.isPlaying:
+            self.mediaplayer.pause()
+            self.isPlaying = False
         else:
-            self.timer.start(30)
-            self.player.set_pause(False)
-            
-    
-    def reset_vid(self):
-        self.vidcap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-        self.player.seek(0, relative = False, accurate = True)
-        self.next_frame()
-        self.timer.start(int(1000/self.vidcap.get(cv2.CAP_PROP_FPS)))
+            self.mediaplayer.play()
+            self.isPlaying = True
 
-    def next_frame(self):
-        ret, frame = self.vidcap.read()
-        if not ret:
-            self.timer.stop()
-            return
-        try:
-            audio_frame, val = self.player.get_frame()
-        except Exception as e:
-            print("error")
-            return
-        
-        cur_img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        height, width, channel = cur_img.shape 
-        qImg = QImage(cur_img.data, width, height, width * channel, QImage.Format_RGB888)
-        self.label.setPixmap(QPixmap.fromImage(qImg))
+
+    def reset_vid(self):
+        self.mediaplayer.set_position(0)
+
 
     def close(self, event):
-        self.vidcap.release()
-        self.player.close_player()
         super().closeEvent(event)
         event.accept()
-
-
-
